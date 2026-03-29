6df2c8aeb8062bd2792173a6772e891f08cbca00
Gabriel <gabrielfranz31@gmail.com>
2026-03-29 17:12:34 +0200

    fix: remove redundant import of Level in FolderLinkDialog

--- src/main/java/com/tonikelope/megabasterd/FolderLinkDialog.java
+++ src/main/java/com/tonikelope/megabasterd/FolderLinkDialog.java
@@ -20,6 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import static java.util.logging.Level.SEVERE;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JComponent;
 import javax.swing.JOptionPane;
